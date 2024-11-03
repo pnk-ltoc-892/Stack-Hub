48cf2f20d43ba6c09e8564dc359ab1986c1897c6
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2024-11-03 18:00:35 +0530

    Add Routing, Layout & Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from './components/ui/button.jsx'
+import { Route, Routes } from 'react-router-dom'
+import AuthLayout from './components/auth/Layout.jsx'
+import AuthLogin from './pages/auth/Login.jsx'
+import AuthRegister from './pages/auth/Register.jsx'
+import AdminLayout from './components/admin-view/Layout.jsx'
+import AdminDashboard from './pages/admin-view/Dashboard.jsx'
+import { AdminProducts } from './pages/admin-view/Products.jsx'
+import AdminOrders from './pages/admin-view/Orders.jsx'
+import ShoppingLayout from './components/shopping-view/Layout.jsx'
+import NotFound from './pages/not-found/index.jsx'
+import ShoppingHome from './pages/shopping-view/Home.jsx'
+import ShoppingListing from './pages/shopping-view/Listing.jsx'
+import ShoppingCheckout from './pages/shopping-view/Checkout.jsx'
+import ShoppingAccount from './pages/shopping-view/Account.jsx'
+
 
 
 function App() {
 
   return (
-    <>
-      <Button>Hi Shadcn</Button>
-    </>
+    <div className='flex flex-col overflow-hidden bg-white'>
+      <Routes>
+        <Route path='/auth' element={<AuthLayout />} >
+          <Route path='login' element={<AuthLogin />}/>
+          <Route path='register' element={<AuthRegister />}/>
+        </Route>
+        <Route path='/admin' element={<AdminLayout />} >
+          <Route path='dashboard' element={<AdminDashboard />}/>
+          <Route path='products' element={<AdminProducts />}/>
+          <Route path='orders' element={<AdminOrders />}/>
+        </Route>
+        <Route path='/shop' element={<ShoppingLayout />} >
+          <Route path='home' element={<ShoppingHome />}/>
+          <Route path='listing' element={<ShoppingListing />}/>
+          <Route path='checkout' element={<ShoppingCheckout />}/>
+          <Route path='account' element={<ShoppingAccount />}/>
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/components/shopping-view/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import ShoppingHeader from "./header.jsx";
+
+function ShoppingLayout() {
+    const [openSidebar, setOpenSidebar] = useState(false);
+
+    return (
+        <div className="flex flex-col bg-white overflow-hidden">
+            {/* common header */}
+            <ShoppingHeader />
+            <main className="flex flex-col w-full" >
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default ShoppingLayout;
